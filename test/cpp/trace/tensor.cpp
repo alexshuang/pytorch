d8ca89370eaabae22f60eb12dd9674e66b833b3e
Huang Shuang <nikshuang@sina.com>
2020-05-08 09:03:40 +0000

    add more tests of GEMM.

--- test/cpp/trace/tensor.cpp
+++ test/cpp/trace/tensor.cpp
@@ -7,10 +7,29 @@ using namespace std;
 #define SEQ_LEN		512
 #define EMBEDDING_SIZE	300
 #define HIDDEN_SIZE	1024
+#define STRIDE	32
 
 static void sgemm(void)
 {
   auto a = torch::randn({SEQ_LEN, EMBEDDING_SIZE}).to(torch::kCUDA);
+  auto b = torch::randn({EMBEDDING_SIZE, HIDDEN_SIZE}).to(torch::kCUDA);
+  auto c = torch::matmul(a, b);
+  cout << c.sizes() << endl;
+}
+
+static void stried_sgemm(void)
+{
+  auto a = torch::randn({SEQ_LEN, EMBEDDING_SIZE + STRIDE}).to(torch::kCUDA);
+  a = a.set_(a.storage(), 0, {SEQ_LEN, EMBEDDING_SIZE}, a.strides());
+  auto b = torch::randn({EMBEDDING_SIZE, HIDDEN_SIZE + STRIDE}).to(torch::kCUDA);
+  b = b.set_(b.storage(), 0, {EMBEDDING_SIZE, HIDDEN_SIZE}, b.strides());
+  auto c = torch::matmul(a, b);
+  cout << c.sizes() << endl;
+}
+
+static void boardcast_sgemm(void)
+{
+  auto a = torch::randn({BS, SEQ_LEN, EMBEDDING_SIZE}).to(torch::kCUDA);
   auto b = torch::randn({EMBEDDING_SIZE, HIDDEN_SIZE}).to(torch::kCUDA);
   auto c = torch::matmul(a, b);
   cout << c.sizes() << endl;
@@ -26,6 +45,8 @@ static void hgemm(void)
 
 int main(void)
 {
-  sgemm();
+  stried_sgemm();
+  //sgemm();
+//  boardcast_sgemm();
   return 0;
 }
