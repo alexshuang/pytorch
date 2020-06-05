bc4d17f3a015b1cc41eb306a2bc42d22dc26663c
Huang Shuang <nikshuang@sina.com>
2020-06-05 06:35:45 +0000

    develop padded batched gemm

--- aten/src/ATen/native/LinearAlgebra.cpp
+++ aten/src/ATen/native/LinearAlgebra.cpp
@@ -458,35 +458,15 @@ Tensor matmul(
     std::vector<int64_t> tensor2_bmm_view({expand_batch_product});
     tensor2_bmm_view.insert(tensor2_bmm_view.end(), {m2, p});
 
-    char transpose_t1, transpose_t2;
-    int64_t t1_paddings, t2_paddings;
-    Tensor t1;
-
-    transpose_t1 = (tensor1.stride(-1) == 1 && tensor1.stride(-2) != 0) ? 'n' : 't';
-    transpose_t2 = (tensor2.stride(-1) == 1 && tensor2.stride(-2) != 0) ? 'n' : 't';
-    t1_paddings = tensor1.stride((transpose_t1 == 'n' ? -2 : -1)) - tensor1.size((transpose_t1 == 'n' ? -1 : -2));
-    t2_paddings = tensor2.stride((transpose_t2 == 'n' ? -2 : -1)) - tensor2.size((transpose_t2 == 'n' ? -1 : -2));
-
     // flatten expanded batches
-#if 0
-/*
-    Tensor tensor1_expanded;
-    Tensor tensor2_expanded;
     int64_t padded_size = p;
-    tensor1_expanded = tensor1.expand(tensor1_expand_size);
-    tensor1_expanded = tensor1_expanded.contiguous();
-    tensor1_expanded = tensor1_expanded.view(tensor1_bmm_view);
+    char transpose_t1 = (tensor1.stride(-1) == 1 && tensor1.stride(-2) != 0) ? 'n' : 't';
+    char transpose_t2 = (tensor2.stride(-1) == 1 && tensor2.stride(-2) != 0) ? 'n' : 't';
+    int64_t t1_paddings = tensor1.stride((transpose_t1 == 'n' ? -2 : -1)) - tensor1.size((transpose_t1 == 'n' ? -1 : -2));
+    int64_t t2_paddings = tensor2.stride((transpose_t2 == 'n' ? -2 : -1)) - tensor2.size((transpose_t2 == 'n' ? -1 : -2));
     if (t1_paddings > 0 && t1_paddings == t2_paddings) {
-      // fixme: check there are no paddings in dim0.
-      tensor1_expanded = tensor1.expand(tensor1_expand_size).view(tensor1_bmm_view);
-      tensor2_expanded = tensor2.expand(tensor2_expand_size).view(tensor2_bmm_view);
       padded_size += t1_paddings;
-    } else {
-      tensor1_expanded = tensor1.expand(tensor1_expand_size).contiguous().view(tensor1_bmm_view);
-      tensor2_expanded = tensor2.expand(tensor2_expand_size).contiguous().view(tensor2_bmm_view);
     }
-*/
-#endif
     Tensor tensor1_expanded = tensor1.expand(tensor1_expand_size).contiguous().view(tensor1_bmm_view);
     Tensor tensor2_expanded = tensor2.expand(tensor2_expand_size).contiguous().view(tensor2_bmm_view);
 
@@ -499,7 +479,7 @@ Tensor matmul(
       output_shape.push_back(p);
     }
 
-#if 0
+#if 1
 	if (has_out) {
         out.resize_({ tensor1_bmm_view[0], tensor1_bmm_view[1], padded_size}).narrow(-1, 0, p);
     } else {
@@ -507,10 +487,6 @@ Tensor matmul(
     }
 
     Tensor output = at::_unsafe_view(at::bmm_out(out, tensor1_expanded, tensor2_expanded), output_shape);
-/*
-    Tensor output = has_out ? at::_unsafe_view(at::bmm_out(out, tensor1_expanded, tensor2_expanded), output_shape)
-                            : at::_unsafe_view(tensor1_expanded.bmm(tensor2_expanded), output_shape);
-*/
 #else
     Tensor output = has_out ? at::_unsafe_view(at::bmm_out(out, tensor1_expanded, tensor2_expanded), output_shape)
                             : at::_unsafe_view(tensor1_expanded.bmm(tensor2_expanded), output_shape);
--- examples/padded_batched_sgemm.py
+++ examples/padded_batched_sgemm.py
@@ -1,24 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
-# NT
-a = torch.randn(12, 6, 500, 300).cuda()
-b = torch.randn(12, 6, 1000, 300).cuda()
-aa, bb = a.clone().detach(), b.clone().detach()
-a = F.pad(a, (0, 32)).narrow(-1, 0, a.size(-1))
-b = F.pad(b, (0, 32)).narrow(-1, 0, b.size(-1))
-print("A size: {}, A stride: {}".format(a.size(), a.stride()))
-print("B size: {}, B stride: {}".format(b.size(), b.stride()))
-c = a @ b.transpose(2, 3)
-print("NT: C size: {}, C stride: {}".format(c.size(), c.stride()))
-print("A size: {}, A stride: {}".format(aa.size(), aa.stride()))
-print("B size: {}, B stride: {}".format(bb.size(), bb.stride()))
-cc = aa @ bb.transpose(2, 3)
-print("NT: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
-
-'''
 # NN
 a = torch.randn(12, 6, 500, 300).cuda()
 b = torch.randn(12, 6, 300, 1000).cuda()
@@ -30,7 +13,22 @@ print("B size: {}, B stride: {}".format(b.size(), b.stride()))
 c = a @ b
 print("NN: C size: {}, C stride: {}".format(c.size(), c.stride()))
 cc = aa @ bb
-print("NN: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
+print(c[0, 0, 0, :5], cc[0, 0, 0, :5])
+#print("NN: c == cc: {}".format(torch.allclose(c, cc)))
+
+# NT
+a = torch.randn(12, 6, 500, 300).cuda()
+b = torch.randn(12, 6, 1000, 300).cuda()
+aa, bb = a.clone().detach(), b.clone().detach()
+a = F.pad(a, (0, 32)).narrow(-1, 0, a.size(-1))
+b = F.pad(b, (0, 32)).narrow(-1, 0, b.size(-1))
+print("A size: {}, A stride: {}".format(a.size(), a.stride()))
+print("B size: {}, B stride: {}".format(b.size(), b.stride()))
+c = a @ b.transpose(2, 3)
+print("NT: C size: {}, C stride: {}".format(c.size(), c.stride()))
+cc = aa @ bb.transpose(2, 3)
+print(c[0, 0, 0, :5], cc[0, 0, 0, :5])
+#print("NT: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
 
 # TN
 a = torch.randn(12, 6, 300, 500).cuda()
@@ -40,10 +38,11 @@ a = F.pad(a, (0, 32)).narrow(-1, 0, a.size(-1))
 b = F.pad(b, (0, 32)).narrow(-1, 0, b.size(-1))
 print("A size: {}, A stride: {}".format(a.size(), a.stride()))
 print("B size: {}, B stride: {}".format(b.size(), b.stride()))
-c = a.T @ b
+c = a.transpose(-1, -2) @ b
 print("TN: C size: {}, C stride: {}".format(c.size(), c.stride()))
-cc = aa.T @ bb
-print("TN: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
+cc = aa.transpose(-1, -2) @ bb
+print(c[0, 0, 0, :5], cc[0, 0, 0, :5])
+#print("TN: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
 
 # TT
 a = torch.randn(12, 6, 300, 500).cuda()
@@ -53,8 +52,8 @@ a = F.pad(a, (0, 32)).narrow(-1, 0, a.size(-1))
 b = F.pad(b, (0, 32)).narrow(-1, 0, b.size(-1))
 print("A size: {}, A stride: {}".format(a.size(), a.stride()))
 print("B size: {}, B stride: {}".format(b.size(), b.stride()))
-c = a.T @ b.T
+c = a.transpose(-1, -2) @ b.transpose(-1, -2)
 print("TT: C size: {}, C stride: {}".format(c.size(), c.stride()))
-cc = aa.T @ bb.T
-print("TT: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
-'''
+cc = aa.transpose(-1, -2) @ bb.transpose(-1, -2)
+print(c[0, 0, 0, :5], cc[0, 0, 0, :5])
+#print("TT: c == cc: {}".format(torch.allclose(c.cpu(), cc.cpu())))
